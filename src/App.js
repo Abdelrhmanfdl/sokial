0c0780407cb6277755cd6f5a4ffa888b362ef2dc
Abdelrhmanfdl <abdofdl99@gmail.com>
2021-01-26 07:57:27 +0200

    update App & add url guard & turn to class-comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import "./App.scss";
 import Signup from "./components/signup";
 import Login from "./components/login";
 import { Link, Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Profile from "./components/profile";
+import Posting from "./components/posting";
+
+import { useState, Component } from "react";
 
 const Sokial = (props) => {
   return (
@@ -11,27 +15,137 @@ const Sokial = (props) => {
           await fetch("/logout", {
             method: "post",
           });
-          window.location.replace("/login");
+          props.loggedOut();
         }}
       >
         Logout
       </button>
       <br />
-      <h3>Welcome to home</h3>
+      <h3>
+        Welcome to home, {props.identity.firstName} {props.identity.lastName}
+      </h3>
     </>
   );
 };
 
-function App() {
+/* function App() {
+  // Check whether Auth or not, and get userData if so
+
+  const getIdentity = async () => {
+    const res = await fetch("/about-auth", { method: "GET" });
+    res = res.json();
+    if (res.auth == false) {
+      // Not authenticated
+    } else {
+    }
+  };
+
+  const [identity, setIdentity] = useState(null);
+  console.log(identity);
+
+  const gotUserData = (data) => {
+    setIdentity(data);
+  };
+
   return (
     <Router>
       <Switch>
-        <Route path="/home" component={Sokial} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        <Route
+          path="/login"
+          component={() => <Login gotUserData={gotUserData} />}
+        />
+        <Route
+          path="/signup"
+          component={() => <Signup gotUserData={gotUserData} />}
+        />
+        <Route
+          path="/home"
+          component={() => (
+            <Sokial
+              loggedOut={() => {
+                setIdentity(null);
+              }}
+              identity={identity}
+            />
+          )}
+        />
+        <Route path="/profile" component={Profile} />
+        <Route path="/posting" component={Posting} />
       </Switch>
     </Router>
   );
 }
+ */
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.gotUserData = this.gotUserData.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+
+    this.state = {
+      identity: null,
+      authChecked: false,
+    };
+  }
+
+  gotUserData(data) {
+    this.setState({ identity: data });
+    window.location.replace("/home");
+  }
+  handleLogout() {
+    window.location.replace("/login");
+    this.setState({ identity: null });
+  }
+
+  async componentDidMount() {
+    let res = await fetch("/about-auth", { method: "GET" });
+    res = await res.json();
+    console.log(res);
+
+    if (res.auth) {
+      if (
+        window.location.pathname == "/login" ||
+        window.location.pathname == "/signup"
+      )
+        window.location.replace("/home");
+      this.setState({ identity: res.userData });
+    } else {
+      if (window.location.pathname == "/home")
+        window.location.replace("/login");
+    }
+
+    this.setState({ authChecked: true });
+  }
+
+  render() {
+    if (this.state.authChecked)
+      return (
+        <Router>
+          <Switch>
+            <Route
+              path="/login"
+              component={() => <Login gotUserData={this.gotUserData} />}
+            />
+            <Route
+              path="/signup"
+              component={() => <Signup gotUserData={this.gotUserData} />}
+            />
+            <Route
+              path="/home"
+              component={() => (
+                <Sokial
+                  loggedOut={this.handleLogout}
+                  identity={this.state.identity}
+                />
+              )}
+            />
+            <Route path="/profile" component={Profile} />
+            <Route path="/posting" component={Posting} />
+          </Switch>
+        </Router>
+      );
+    else return null;
+  }
+}
 export default App;
