d90846e5e30fc8090cbbc5a489785e1f50822ec6
Abdelrhmanfdl <abdofdl99@gmail.com>
2021-02-19 10:38:32 +0200

    add profile posts option component

---
+++ src/components/profilePostsOption.js
@@ -0,0 +1,56 @@
+import { TextField, TextareaAutosize, Grid, Button } from "@material-ui/core";
+import Posting from "./posting";
+import PostsProfileSection from "./postsProfileSection";
+import { useState, useEffect } from "react";
+
+const ProfilePostsOption = (props) => {
+  // document.body.onscroll = null;
+  return (
+    <Grid
+      id="profile-two-colums"
+      direction="row"
+      justify={"space-evenly"}
+      container
+    >
+      <Grid id="profile-left-column" item sm={4} xs={12}>
+        <div
+          id="profile-posts-about-section"
+          className="profile-posts-section"
+          item
+        >
+          About
+        </div>
+        <div
+          id="profile-posts-photos-section"
+          className="profile-posts-section"
+          item
+        >
+          Photos
+        </div>
+        <div
+          id="profile-posts-friends-section"
+          className="profile-posts-section"
+          item
+        >
+          Friends
+        </div>
+      </Grid>
+
+      <Grid id="profile-right-column" item sm={7} lg={7} xs={12}>
+        <Posting
+          identity={props.identity}
+          isMyProfile={props.isMyProfile}
+          profileData={props.profileData}
+        />
+        <PostsProfileSection
+          identity={props.identity}
+          isMyProfile={props.isMyProfile}
+          profileId={props.profileData.id}
+          profileData={props.profileData}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ProfilePostsOption;
